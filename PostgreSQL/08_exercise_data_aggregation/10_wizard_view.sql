c5ec543eff9d3798fc3436791b41fae201235910
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2024-05-27 14:20:06 +0100

    add exercise data agg

---
+++ PostgreSQL/08_exercise_data_aggregation/10_wizard_view.sql
@@ -0,0 +1,13 @@
+CREATE VIEW 
+	view_wizard_deposits_with_expiration_date_before_1983_08_17
+AS SELECT DISTINCT
+	first_name || ' ' || last_name AS "Wizard Name",
+	deposit_start_date AS "Start Date",
+	deposit_expiration_date AS "Expiration Date",
+	deposit_amount AS "Amount"
+FROM 
+	wizard_deposits
+WHERE 
+	deposit_expiration_date <= '1983-08-17'
+ORDER BY
+	deposit_expiration_date ASC;
