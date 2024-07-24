e176c9f4c998288fc4704a1fc22e416d80211468
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2024-07-24 19:04:52 +0100

    update caller.py

--- PythonORM/exam-prep-2/caller.py
+++ PythonORM/exam-prep-2/caller.py
@@ -1,10 +1,53 @@
 import os
 import django
 
-# Set up Django
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from django.db.models import Q
 
-# Create queries within functions
+from main_app.models import Profile, Order
+
+
+def get_profiles(search_string=None):
+    if search_string is None:
+        return ''
+
+    profiles = Profile.objects.filter(
+        Q(full_name__icontains=search_string)
+        |
+        Q(email__icontains=search_string)
+        |
+        Q(phone_number__icontains=search_string)
+    ).order_by('full_name')
+
+    if not profiles.exists():
+        return ""
+
+    return "\n".join(
+        f"Profile: {p.full_name}, email: {p.email}, phone number: {p.phone_number}, orders: {p.orders.count()}"
+        for p in profiles
+    )
+
+def get_loyal_profiles() -> str:
+    profiles = Profile.objects.get_regular_customers()
+
+    if not profiles.exists():
+        return ""
+
+    return "\n".join(
+        f"Profile: {p.full_name}, orders: {p.orders.count()}"
+        for p in profiles
+    )
+
+def get_last_sold_products() -> str:
+    last_order = Order.objects.prefetch_related('products').last()
+
+    if last_order is None or not last_order.products.exists():
+        return ""
+
+
+    products = ', '.join(last_order.products.order_by('name').values_list('name', flat=True))
+
+    return f"Last sold products: {products}"
